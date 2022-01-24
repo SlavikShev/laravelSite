760e77742d0d3a7d5437712d6ae3738ae0cfe8fb
vyacheslav.shevchuk <12061998s>
2022-01-24 20:38:06 +0200

    created controller for post categories and route for it

---
+++ app/Http/Controllers/Admin/PostCategoryController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\PostCategory;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PostCategoryController extends Controller {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index() {
+        $categories = PostCategory::all();
+        return view('categories.index',compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return View
+     */
+    public function create() {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     */
+    public function store(Request $request) {
+        $data = $request->all();
+        $title = $data['title'];
+
+        $result = PostCategory::create([
+            'title'=>$title
+        ]);
+        if ($result)
+            return redirect()
+                ->route('categories.index')
+                ->with(['success' => 'Категория успешно сохранена']);
+        else
+            return back()
+                ->withErrors(['error'=>'Ошибка при сохранении'])
+                ->withInput();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/categories', 'PostCategoryController@index');
+Route::resource('categories','Admin\PostCategoryController');
